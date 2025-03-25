ed92705d86f069358767af2b60ef21ec3eeb5caf
Aadu <168184699+aaduexe@users.noreply.github.com>
2025-03-25 15:44:54 +0530

    Basic terminal based application

---
+++ source/main.py
@@ -0,0 +1,42 @@
+from dataLogic import DataHandler
+from userInput import newItem
+import bareBone as bb
+from datetime import date
+from datetime import datetime
+
+while True:
+    table = DataHandler()
+    initialization = input("Do you want to add new item or revisit item from the list?\nInput 'A' to Add Item | Input 'R' to Revisit item | Input 'Q' to Quit\n")
+    if initialization.lower() == 'a':
+        newItem = newItem()
+        table.dataUpdate(newItem)
+        table.saveData()
+        userInput1 = input(" Your item is added.\nInput 'R' to Re-run the prompts | Input 'Q' to Quit\n")
+        if userInput1.lower() == 'r':
+            pass
+        elif userInput1.lower() == 'q':
+            break
+        else:
+            print('Your itme is succesfullu added. No valid input, prompts restarted.')
+    elif initialization.lower() == 'r':
+        print(table.data)
+        desiredItem_key = input("Type name of item you want to buy.\n")
+
+        dateRec = table.data.loc[table.data['item'] == desiredItem_key, 'date'].values[0]
+        days = (date.today() - datetime.strptime(str(dateRec), "%Y-%m-%d").date()).days
+
+        if days > 29:
+            questionScore = bb.AskSession()
+            status_value = bb.Conclusion(questionScore)
+            table.statusUpdate(desiredItem_key, status_value)
+            table.saveData()
+            print("The status of",desiredItem_key,"is updated to",status_value,"as per the answers.")
+        else:
+            if days == 1:
+                print("It's been",days,"day only. Please wait for at-least 30 days.")
+            else:
+                print("It's been",days,"days only. Please wait for at-least 30 days.")
+    elif initialization.lower() == 'q':
+        break
+    else:
+        print('No valid input recognized')
--- source/userInput.py
+++ source/userInput.py
@@ -1,13 +1,10 @@
-from dataLogic import DataHandler
 from datetime import date
 
-item = input("What item are you looking to buy?\n")
-new_row = {
-    'date' : date.today(),
-    'item' : item,
-    'status' : 'waiting'
-}
-
-dataFrame = DataHandler()
-dataFrame.dataUpdate(new_row)
-dataFrame.saveData()
+def newItem():
+    item = input("What item are you looking to buy?\n")
+    new_row = {
+        'date' : date.today(),
+        'item' : item,
+        'status' : 'waiting'
+    }
+    return new_row
