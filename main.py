5a34157c35ded4d8aa8b835ed0f0ec9055abdd91
Aadu <168184699+aaduexe@users.noreply.github.com>
2025-04-20 12:35:48 +0530

    IntroducedHtml

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def startHere():
+    return render_template("index.html")
+
+@app.route("/adder")
+def addItem():
+    return render_template("adder.html")
+
+@app.route("/decide")
+def dataDropDown():
+    column_data = []
+    with open('data/wishes.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            column_data.append(row['Wish'])  # Change 'City' to your column name
+
+    return render_template("decider.html", items=column_data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
