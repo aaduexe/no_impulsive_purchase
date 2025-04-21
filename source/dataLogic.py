f044c081231c98762aef74daa55b89a90aaec2e6
Aadu <168184699+aaduexe@users.noreply.github.com>
2025-04-21 16:52:09 +0530

    Review Logic added

--- source/dataLogic.py
+++ source/dataLogic.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import os
+import source.bareBone as bb
+from datetime import date
+from datetime import datetime
 
 class DataHandler:
     def __init__(self):
@@ -33,6 +36,23 @@ class DataHandler:
             os.makedirs('data')
         
         self.data.to_csv(self.file_path, index=False)
+    
+    def review_item(self, desiredItem_key: str) -> str:
+        dateRec = self.data.loc[self.data['Wish'] == desiredItem_key, 'Date'].values[0]
+        days = (date.today() - datetime.strptime(str(dateRec), "%Y-%m-%d").date()).days
+        if days > 29:
+            questionScore = bb.AskSession()
+            status_value = bb.Conclusion(questionScore)
+            self.update_status(desiredItem_key, status_value)
+            self.save_to_csv()
+        else:
+            if days == 1:
+                show = f"It's been {days} day only. Please wait for at-least 30 days."
+                print("It's been",days,"day only. Please wait for at-least 30 days.")
+            else:
+                show = f"It's been {days} days only. Please wait for at-least 30 days."
+                print(f"It's been {days} days only. Please wait for at-least 30 days.")
+            return show
 
     def update_status(self, wish: str, status: str) -> None:
         """
